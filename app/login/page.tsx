896acd489d3e1ce35bc7a934f5f8041ccea8543a
lnoueryo <jounetsism@gmail.com>
2025-11-30 00:20:38 +0900

    fix: nextのパラメーター修正

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,7 @@
 import Login from "@/components/organisms/Login";
 
-export default function LoginPage({
-  searchParams,
-}: {
-  searchParams: { next?: string };
-}) {
-  const next = searchParams.next || "/";
+export default async function LoginPage({ searchParams }: { searchParams: Promise<{ next?: string }> }) {
+  const params = await searchParams;
+  const next = params.next ?? '/';
   return <Login next={next} />;
 }
