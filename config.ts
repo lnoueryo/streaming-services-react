ff3d33f0708506c89858feb34e6a76d4a04cd403
lnoueryo <inoue@withlive.jp>
2025-12-29 00:07:23 +0900

    feat: stgの環境作成

--- config.ts
+++ config.ts
@@ -14,7 +14,7 @@ type ConfigEnv = {
   loggerLevel: string
   domain: string
 }
-type STAGE = 'development' | 'production'
+type STAGE = 'development' | 'staging' | 'production'
 
 const configEnvs: { [K in STAGE]: ConfigEnv } = {
   development: {
@@ -27,6 +27,16 @@ const configEnvs: { [K in STAGE]: ConfigEnv } = {
     loggerLevel: 'debug',
     domain: 'localtest.me',
   },
+  staging: {
+    streamingApiFrontendOrigin: 'https://streaming.staging.biz:8443',
+    streamingBackendApiOrigin: {
+      client: 'https://streaming-api.staging.biz:8443',
+      server: 'http://streaming-backend-stg:4000',
+    },
+    signalingOrigin: 'wss://streaming-signaling.staging.biz:8443',
+    loggerLevel: 'debug',
+    domain: 'staging.biz',
+  },
   production: {
     streamingApiFrontendOrigin: 'https://streaming.jounetsism.biz',
     streamingBackendApiOrigin: {
@@ -38,8 +48,8 @@ const configEnvs: { [K in STAGE]: ConfigEnv } = {
     domain: 'jounetsism.biz',
   },
 }
-const env = (process.env.NODE_ENV || 'development') as STAGE
-const envList = ['development', 'production']
+const env = (process.env.NEXT_PUBLIC_APP_ENV || 'development') as STAGE
+const envList = ['development', 'staging', 'production']
 if (!envList.includes(env)) {
   throw new Error('invalid STAGE')
 }
